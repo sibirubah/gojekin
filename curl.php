4dd0c679df8cfe182c22ab0b9f90e217f28dc57c
AndoniMarp <32451205+Andoni444@users.noreply.github.com>
2019-11-19 09:01:11 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -149,7 +149,7 @@ function verif($otp, $token)
     }
 function claim($token)
     {
-    $data = '{"promo_code":"GOFOODBOBA07"}';    
+    $data = '{"promo_code":"GOFOODSANTAI19"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
     if ($claim['success'] == 1)
         {
